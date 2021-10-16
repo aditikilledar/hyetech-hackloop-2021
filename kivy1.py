367c2ed78af4d5a421eecbc9e8ce5d1686d3075b
Aditi Killedar <aditi.s.killedar@gmail.com>
2021-10-16 19:56:54 +0530

    updated

--- kivy1.py
+++ kivy1.py
@@ -1,4 +1,3 @@
-__author__ = 'bunkus'
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.boxlayout import BoxLayout
--- landing.py
+++ landing.py
@@ -8,22 +8,55 @@ from kivy.uix.button import Button
 # boxLayout
 from kivy.uix.boxlayout import BoxLayout
 
-
 kivy.require('2.0.0')
-
-# class HelloWorld(App):
-
-# 	def build(self):
-# 		return Label(text = "HIIIIIIIIIIIII")
-
-
-# HelloWorld().run()
-
 class LargestBox(App):
 	# for the largest container that 
 	def build(self):
 
 		superBox = BoxLayout(orientation='vertical')
 
+		# code to show how to use nested boxlayouts.
+# class in which we are creating the button by using boxlayout
+# defining the App class
+class BoxLayoutApp(App):
+	
+	def build(self):
 
+		# To position oriented widgets again in the proper orientation
+		# use of vertical orientation to set all widgets
+		superBox = BoxLayout(orientation ='vertical')
 		
+
+		camBox = BoxLayout(orientation = 'vertical')
+		btncam = Label(text ="yo face")
+
+		camBox.add_widget(btncam)
+
+		# To position widgets next to each other,
+		# use a horizontal BoxLayout.
+		bottomBox = BoxLayout(orientation ='horizontal')
+		resultLabel = Label(text ="Left/Right")
+		btn2 = Button(text ="Start")
+
+		# HB represents the horizontal boxlayout orientation
+		# declared above
+		bottomBox.add_widget(resultLabel)
+		bottomBox.add_widget(btn2)
+
+		# To position widgets above/below each other,
+		# use a vertical BoxLayout.
+		# superbox used to again align the oriented widgets
+		superBox.add_widget(camBox)
+		superBox.add_widget(bottomBox)
+
+		return superBox
+
+# creating the object root for BoxLayoutApp() class
+root = BoxLayoutApp()
+
+# run function runs the whole program
+# i.e run() method which calls the
+# target function passed to the constructor.
+if __name__ == "__main__":
+	root.run()
+
